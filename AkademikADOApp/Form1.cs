932ac97626a8f048c48b4a2e39dda961ab4b3ece
ramdaniasyafitriani <ramdaniania04@gmail.com>
2026-03-10 13:25:43 +0800

    mendeklarasikan objek koneksi

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -9,6 +9,9 @@ namespace AkademikADOApp
         // Connection  string
         string connString = "Data Source=LAPTOP-M4FI536S\\RAMDANIAA;Initial Catalog=DBAkademikADO;Integrated Security=True";
 
+        //Object SQL Connection
+        SqlConnection conn;
+
         public Form1()
         {
 
